64e1ac3ce2b211e7428aad349e1a76b199448d21
fasaldana <fernando_s_98@hotmail.com>
2024-01-23 21:17:38 -0500

    Updated to create a layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
+import Navbar from "~/components/Navbar";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Condorsoft</title>
+        <meta name="description" content="Condorsoft technical test" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center bg-fixed bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/bgImage.png')" }}>
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 };
 
 export default MyApp;
