225d2a64eee2678c3d6a20a53585c0c90471bf33
fasaldana <fernando_s_98@hotmail.com>
2024-01-23 21:33:19 -0500

    Add logic to authenticate a user on the db

---
+++ src/pages/api/auth/signin.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type User = {
+    name: string;
+    email: string;
+    password: string;
+};
+
+const prisma = new PrismaClient();
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    const { name, email, password } = req.body as User;
+
+    const createdUser = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password,
+        },
+    });
+
+
+    console.log(createdUser);
+
+    res.status(200).json(createdUser);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`,
+    );
+  }
+}
