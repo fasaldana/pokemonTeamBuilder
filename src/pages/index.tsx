5d2d039a3621bda1f5860696a16d1202a08fdc05
fasaldana <fernando_s_98@hotmail.com>
2024-01-23 21:16:38 -0500

    Fix checkbox functionality and changed home to be a login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,83 @@
-import Head from "next/head";
-import Navbar from "~/components/Navbar";
+import Link from "next/link";
 import { useEffect, useState } from "react";
-import PokeCard from "~/components/PokeCard";
-import SelectedPoke from "~/components/SelectedPoke";
-import Image from "next/image";
+import Login from "~/components/Login";
+import SignIn from "./signIn";
 
 export default function Home() {
-  const [pokemon, setPokemon] = useState([]);
-  const [selectedPokemon, setSelectedPokemon] = useState<{ image: string; name: string; }[]>([]); // Add initial value for selectedPokemon
+
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const request = async () => {
-      const result = await fetch("/api/pokemon");
-      // This is just an example to obtain data from the endpoint. Hint :) avoid no typesafety we hate that
-      const resultJson = await result.json();
-      setPokemon(resultJson);
-    };
-    void request();
+    const user = localStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
+    }
   }, []);
 
-  const handleSelectedPoke = (image: string, name: string) => {
-    if (selectedPokemon.length < 6) {
-      setSelectedPokemon([...selectedPokemon, { image, name }]);
-    } else {
-      alert("You can only select 6 pokemon");
+  const handleLogin = async (email, password) => {
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      const result = await response.json();
+      setUser(result);
+      localStorage.setItem("user", JSON.stringify(result));
+    } catch (error) {
+      console.log(error);
     }
-  };
+  }
+
+  const handleSignIn = async (name: string, email: string, password: string) => {
+    console.log("Sign In");
+    try {
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      });
+      const result = await response.json();
+      setUser(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleSignOut = async () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  }
 
   return (
-    <>
-      <Head>
-        <title>Condorsoft</title>
-        <meta name="description" content="Condorsoft technical test" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex min-h-screen flex-col items-center bg-fixed bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/bgImage.png')" }}>
-        <Navbar />
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
-          {pokemon.map((poke) => (
-            <PokeCard
-              key={poke.id}
-              id={poke.id}
-              name={poke.name}
-              image={poke.image}
-              types={poke.type}
-              handleSelectedPoke={handleSelectedPoke}
-            />
-          ))}
+    <div className="flex flex-col justify-center items-center bg-zinc-800 rounded-lg shadow-lg p-4 m-4 h-full">
+      <h1 className="text-4xl text-center text-white">Welcome to the Pokedex</h1>
+      {user ? (
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl text-white">Welcome {user.name}</h2>
+          <button className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </button>
         </div>
-        <div className="fixed bottom-0 mb-4 mr-4 w-full flex justify-center">
-          <div className="flex space-x-2 gap-10">
-            {selectedPokemon.length > 0 ? (
-                selectedPokemon.map((poke) => (
-                  <SelectedPoke key={poke.name} image={poke.image} name={poke.name} />
-                ))
-              ) : (
-              <div className="flex flex-col justify-center items-center bg-zinc-800 rounded-lg shadow-lg p-4 m-4">
-                <h2 className="text-lg text-white">Select Pokemon for your team</h2>
-              </div>
-            )}
-          </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center">
+          <Login handleLogin={handleLogin} />
+          <SignIn handleSignIn={handleSignIn} />
         </div>
-      </main>
-    </>
-  );
+      )}
+    </div>
+  )
 }
