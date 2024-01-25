c0800145e635dfa7b9c332a2d15318faf06ddea1
fasaldana <fernando_s_98@hotmail.com>
2024-01-25 02:43:02 -0500

    Update navbar made logo bigger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="flex w-full flex-col items-center justify-center bg-black bg-opacity-70 px-4 py-8">
-      <Image src="/logo.png" width={100} height={100} alt="logo" />
+      <Image src="/logo.png" width={150} height={150} alt="logo" />
       <div className="mt-4 flex gap-14 space-x-4 text-base text-white">
         <Link href="/">Home</Link>
         <Link href="/pokedex">Pokedex</Link>
